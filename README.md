44a1059cb1aedd219386e48a2668b1d10bff7861
woosanggyu <wsg0701@naver.com>
2021-05-01 11:43:35 +0900

    Action and storeDispatch

--- README.md
+++ README.md
@@ -15,3 +15,7 @@
 5. 오직 Reducer에서만 Data를 modify할 수 있다.
 
 6. Reducer가 리턴하는 것은 application에 있는 data가 된다.
+
+7. action은 redux에서 function을 부를 때 쓰는 두번째 parameter or argument로 reducer와 소통하기 위한 방법이다.
+
+8. dispatch를 통해 reducer에게 메세지(Object)를 전달할 수 있다. ex)store.dispatch({ key : value })
--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ const plus = document.getElementById("Plus");
 const minus = document.getElementById("Minus")
 const num = document.querySelector("span")
 
-const countModifier = (count = 0) => {
-    
-    return count;
+const countModifier = (count = 0, action ) => {
+    if(action.type === "Add") {
+        return count + 1;
+    } else if (action.type === "Minus") {
+        return count - 1;
+    } else {
+        return count;
+    }
 }
 
 const countStore = createStore(countModifier);
 
-console.log(countStore.getState());
+//오브젝트 형식이어야 함.
+countStore.dispatch({ type : "Add"});
+
+console.log(countStore.getState())
